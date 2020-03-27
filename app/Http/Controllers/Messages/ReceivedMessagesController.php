c483741b35db52c69987a8f36f5a0825385b5209
bengeos <george.beng@gmail.com>
2020-03-27 12:07:14 +0300

    update changes

--- app/Http/Controllers/Messages/ReceivedMessagesController.php
+++ app/Http/Controllers/Messages/ReceivedMessagesController.php
@@ -51,10 +51,10 @@ class ReceivedMessagesController extends Controller
 
     public function createdReceivedMessage()
     {
+        $incoming_message = request()->all();
         try {
-            $incoming_message = request()->all();
             // Create New Received Message
-            if (isset($incoming_message['binary']) && isset($incoming_message['to'])) {
+            if (isset($incoming_message['binary']) && isset($incoming_message['to']) && $incoming_message['id']) {
                 $negaritClient = NegaritClient::where('short_code', '=', $incoming_message['to'])->where('status', '=', true)->first();
                 if ($negaritClient instanceof NegaritClient) {
                     $new_received_message = new ReceivedMessage();
@@ -72,44 +72,53 @@ class ReceivedMessagesController extends Controller
                         dispatch(new SyncReceivedMessageTask($new_received_message));
                         return 'ACK/Jasmin';
                     } else {
-                        print_r($negaritClient);
-                        exit();
+                        return response()->json(['status' => false, 'message' => 'Whoops! Unable to save this message'], 500);
                     }
+                } else {
+                    return response()->json(['status' => false, 'message' => 'Negarit Client Not Found For This'], 500);
                 }
-
             } // Create Delivery Report
             elseif (isset($incoming_message['message_status']) && isset($incoming_message['id'])) {
                 // Create Type 1 Deliver Report
                 if (isset($incoming_message['donedate']) && isset($incoming_message['id_smsc'])) {
                     $newDeliveryMessage = new DeliveryReport();
                     $newDeliveryMessage->delivery_type = DeliveryReport::DELIVERY_REPORT_TYPE['TYPE_ONE'];
-                    $newDeliveryMessage->message_id = isset($incoming_message['id'])? $incoming_message['id']: null;
-                    $newDeliveryMessage->message_status = isset($incoming_message['message_status'])? $incoming_message['message_status']: null;
-                    $newDeliveryMessage->level = isset($incoming_message['level'])? $incoming_message['level']: null;
-                    $newDeliveryMessage->delivered = isset($incoming_message['dlvrd'])? $incoming_message['dlvrd']: null;
-                    $newDeliveryMessage->error = isset($incoming_message['err'])? $incoming_message['err']: null;
-                    if($newDeliveryMessage->save()) {
+                    $newDeliveryMessage->message_id = isset($incoming_message['id']) ? $incoming_message['id'] : null;
+                    $newDeliveryMessage->message_status = isset($incoming_message['message_status']) ? $incoming_message['message_status'] : null;
+                    $newDeliveryMessage->level = isset($incoming_message['level']) ? $incoming_message['level'] : null;
+                    $newDeliveryMessage->delivered = isset($incoming_message['dlvrd']) ? $incoming_message['dlvrd'] : null;
+                    $newDeliveryMessage->error = isset($incoming_message['err']) ? $incoming_message['err'] : null;
+                    if ($newDeliveryMessage->save()) {
                         dispatch(new SyncDeliveryReportTask($newDeliveryMessage));
                         return 'ACK/Jasmin';
+                    } else {
+                        logger('ReceivedMessagesController', ['message' => 'Whoops! unable to save delivery message', 'data' => $incoming_message]);
+                        return response()->json(['status' => false, 'message' => 'Whoops! unable to save delivery message'], 500);
                     }
                 } elseif (isset($incoming_message['level'])) {
                     // Create Type 2 Deliver Report
                     $newDeliveryMessage2 = new DeliveryReport();
                     $newDeliveryMessage2->delivery_type = DeliveryReport::DELIVERY_REPORT_TYPE['TYPE_TWO'];
-                    $newDeliveryMessage2->message_id = isset($incoming_message['id'])? $incoming_message['id']: null;
-                    $newDeliveryMessage2->message_status = isset($incoming_message['message_status'])? $incoming_message['message_status']: null;
-                    $newDeliveryMessage2->level = isset($incoming_message['level'])? $incoming_message['level']: null;
+                    $newDeliveryMessage2->message_id = isset($incoming_message['id']) ? $incoming_message['id'] : null;
+                    $newDeliveryMessage2->message_status = isset($incoming_message['message_status']) ? $incoming_message['message_status'] : null;
+                    $newDeliveryMessage2->level = isset($incoming_message['level']) ? $incoming_message['level'] : null;
                     $newDeliveryMessage2->delivered = null;
                     $newDeliveryMessage2->error = null;
-                    if($newDeliveryMessage2->save()) {
+                    if ($newDeliveryMessage2->save()) {
                         dispatch(new SyncDeliveryReportTask($newDeliveryMessage2));
                         return 'ACK/Jasmin';
+                    } else {
+                        logger('ReceivedMessagesController', ['message' => 'Whoops! unable to save delivery message', 'data' => $incoming_message]);
+                        return response()->json(['status' => false, 'message' => 'Whoops! unable to save delivery message'], 500);
                     }
                 }
+            } else {
+                logger('ReceivedMessagesController', ['message' => 'Whoops! invalid message format', 'data' => $incoming_message]);
+                return response()->json(['status' => false, 'message' => 'Whoops! Invalid message format'], 500);
             }
         } catch (\Exception $exception) {
-            print_r($exception->getMessage());
-            exit();
+            logger('ReceivedMessagesController', ['Exception' => $exception->getMessage(), 'data' => $incoming_message]);
+            return response()->json(['status' => false, 'message' => 'Whoops! Invalid message', 'exception'=>$exception->getMessage()], 500);
         }
     }
 }
